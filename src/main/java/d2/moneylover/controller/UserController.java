cca9aa1877d604bd26d2fca1f5909e8afc8fa0ee
nguyenthimai5 <mikhailthv@gmail.com>
2023-05-21 17:48:16 +0700

    giaodien

--- src/main/java/d2/moneylover/controller/UserController.java
+++ src/main/java/d2/moneylover/controller/UserController.java
@@ -1,4 +1,27 @@
 package d2.moneylover.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    @GetMapping
+    public String index() {
+        return "/user/index";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "/user/about";
+    }
+
+    @GetMapping("/how")
+    public String how() {
+        return "/user/how";
+    }
+
+    @GetMapping("/wallet")
+    public String wallet() {
+        return "/user/wallet";
+    }
 }
--- src/main/java/d2/moneylover/repository/UserRepository.java
+++ src/main/java/d2/moneylover/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package d2.moneylover.repository;
 
-public interface UserRepository {
+import d2.moneylover.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByName(String name);
 }
